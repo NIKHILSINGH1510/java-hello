c5632325bd7c178b3330732959c6fafdc96b98c6
NIKHILSINGH1510 <56766208+NIKHILSINGH1510@users.noreply.github.com>
2020-10-06 01:02:29 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,13 +1,12 @@
 
 public class java1 {
-    public static void main(String args[]) {
+    public static void main(String args[])
+     {
         System.out.println("This will be printed");
-<<<<<<< Updated upstream
-=======
         System.out.println("This will also be printed");
         System.out.println("Then why not this");
 
 
->>>>>>> Stashed changes
+
     }
 }
