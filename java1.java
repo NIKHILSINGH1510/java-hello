33f36c172e642035e72c69c5ffbacfde2190149b
NIKHILSINGH1510 <201952221@iiitvadodara.ac.in>
2022-07-03 19:48:40 +0530

    Commit

--- java1.java
+++
@@ -1,8 +0,0 @@
-
-public class java1 {
-    public static void main(String args[]) {
-        System.out.println("This will be printed");
-        System.out.println("This will also be printed");
-
-    }
-}
